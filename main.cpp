cde98157feb0d0b56b4e5e1a096d906e13f86b3e
oliver <olivercgrant@gmail.com>
2017-10-06 09:33:42 +0200

    Adding ability to count glycan overlaps

--- main.cpp
+++ main.cpp
@@ -21,7 +21,6 @@ typedef std::vector<Assembly*> AssemblyVector;
 typedef std::vector<Residue*> ResidueVector;
 typedef std::vector<MolecularModeling::Atom*> AtomVector;
 
-
 constexpr auto PI = 3.14159265358979323846;
 constexpr auto CSA = 36.31681103; // Carbon Surface Area for normalizing results to the area of a carbon atom. Can ask the question: How many C atom equivalents are inside the protein?
 
@@ -31,14 +30,18 @@ Residue* recursively_seek_alycone_residue(Atom *start_atom, bool *found);
 double CalculateAtomicOverlapsForAssemblyVector(Assembly *assemblyA, AssemblyVector assemblies);
 bool CheckIfProtein(std::string resname);
 double SuperimposeSnapshotsToEnzymeCalculatePercentTimeAccessible(Assembly *enzyme, Assembly *enzyme_ligand, AssemblyVector substrate_snapshots, std::string substrate_residue_number);
-//double SuperimposeEnzymeCalculateOverlaps(Assembly *enzyme, AtomVector target_atoms, AtomVector substrate, AtomVector substrate_superimposition_atoms);
+void BuildStructuresInParallel(AssemblyVector &assemblies);
 AtomVector GetAtomSelection(Assembly *assembly, StringVector residue_numbers, StringVector atom_names);
+AtomVector GetAtomsCommonToBothAtomVectors(AtomVector a, AtomVector b);
+AtomVector GetAtomsin_a_Notin_b_AtomVectors(AtomVector a, AtomVector b);
+AtomVector seek_attached_residues_that_are_not_protein(std::string residue_number, Assembly *assembly);
+void recursively_find_attached_residues_that_are_not_protein(Atom *start_atom, AtomVector &returned_selection);
+
 
 int main(int argc, char *argv[])
 {
     std::string working_Directory = Find_Program_Working_Directory();
     std::string installation_Directory = Find_Program_Installation_Directory();
-
 
     //************************************************//
     // Reading input file                             //
@@ -75,10 +78,14 @@ int main(int argc, char *argv[])
     conversion_map.PrintMap();
 
     if (fileExists(parameter_directory + "/amino12.lib"))
+    {
         std::cout << "Using user provided parameters" << std::endl;
+    }
     else
+    {
         std::cout << "Using default parameters from installation folder" << std::endl;
         parameter_directory = installation_Directory + "/CurrentParams";
+    }
 
     //************************************************//
     // Details for loading in a PDB file              //
@@ -153,105 +160,39 @@ int main(int argc, char *argv[])
     }
     closedir( dp );
 
+    for(AssemblyVector::iterator it = substrate_snapshots.begin(); it != substrate_snapshots.end(); ++it)
+    {
+        Assembly *assembly = *it;
+        assembly->BuildStructureByDistance(20);
+    }
 
     //************************************************//
     // Superimposition                                //
     //************************************************//
     std::cout << "Superimposition and overlap calculation" << std::endl;
 
-
-/*
-    //AtomVector alsoMoving_atoms = enzyme.GetAllAtomsOfAssembly();
-    AtomVector target_atoms;
-    AtomVector enzyme_ligand_atoms = enzyme_ligand.GetAllAtomsOfAssembly();
-    for(AtomVector::iterator it = enzyme_ligand_atoms.begin(); it != enzyme_ligand_atoms.end(); ++it)
-    {
-        Atom *atom = *it;
-        if ( (atom->GetName().compare("C1")==0) || (atom->GetName().compare("C3")==0) || (atom->GetName().compare("C5")==0) )
-            target_atoms.push_back(atom);
-    }
-    */
-
-
     std::string converted_resnum;
-    substrate_snapshots.at(0)->BuildStructureByDistance();
+    //substrate_snapshots.at(0)->BuildStructureByDistance();
 
     StringVector split_substrate_residue_numbers = split(substrate_residue_numbers, ',');
     for(StringVector::iterator it = split_substrate_residue_numbers.begin(); it != split_substrate_residue_numbers.end(); ++it)
     {
        // std::cout << "id is " << *it << std::endl;
-
-        double percent_accessible = SuperimposeSnapshotsToEnzymeCalculatePercentTimeAccessible(&enzyme, &enzyme_ligand, substrate_snapshots, *it);
+        AtomVector temp = seek_attached_residues_that_are_not_protein(*it, substrate_snapshots.at(0));
+       /* for(AtomVector::iterator it1 = temp.begin(); it1 != temp.end(); ++it1)
+        {
+            Atom *atom = *it1;
+            std::cout << "This atom is part of glycan: " << atom->GetId() << std::endl;
+        }*/
+        //double percent_accessible = SuperimposeSnapshotsToEnzymeCalculatePercentTimeAccessible(&enzyme, &enzyme_ligand, substrate_snapshots, *it);
 
         Residue* algycone = seek_alycone_residue_connected_to_residue_number(*it, substrate_snapshots.at(0));
         converted_resnum = conversion_map.ConvertViaMap(algycone->GetNumber());
-        std::cout << "Time accessible for " << converted_resnum << ": " <<  percent_accessible << std::endl;
-    }
-/*
-   // int i=0; //counter only for output pdb file names
-    for(AssemblyVector::iterator it = substrate_snapshots.begin(); it != substrate_snapshots.end(); ++it)
-    {     
-        Assembly *assembly = *it;
-        AtomVector alsoMoving_atoms = assembly->GetAllAtomsOfAssembly();
-        ResidueVector residues = assembly->GetAllResiduesOfAssembly();
-        AtomVector super_atoms;
-        for(ResidueVector::iterator itt = residues.begin(); itt != residues.end(); ++itt)
-        {
-            Residue *residue = *itt;
-            StringVector split_id = split(residue->GetId(), '_');
-            if ( substrate_residue_number.compare(split_id.at(2))==0 )
-            {
-                AtomVector atoms = residue->GetAtoms();
-                for(AtomVector::iterator ittt = atoms.begin(); ittt != atoms.end(); ++ittt)
-                {
-                    Atom *atom = *ittt;
-                    if ( (atom->GetName().compare("C1")==0) || (atom->GetName().compare("C3")==0) || (atom->GetName().compare("C5")==0) )
-                        super_atoms.push_back(atom);
-                }
-            }
-        }
-
-        gmml::Superimpose(super_atoms, target_atoms, alsoMoving_atoms);
-
-        //Write out a pdb file:
-       // std::stringstream ss;
-       // ss << working_Directory + "/outputs/moved_" << i << ".pdb";
-       // PdbFileSpace::PdbFile *outputPdbFile = assembly->BuildPdbFileStructureFromAssembly(-1,0);
-       // outputPdbFile->Write(ss.str());
-      //  ++i;
-
-       // double overlaps = CalculateAtomicOverlaps(&enzyme, assembly);
-       // std::cout << CalculateAtomicOverlaps(&enzyme, assembly) << std::endl;
-       // std::ofstream myfile;
-
-      //  std::stringstream ss1;
-      //  ss1 << working_Directory + "/outputs/Overlaps.txt";
-     //   std::cout << ss1.str() << std::endl;
-     //   myfile.open (ss1.str(), std::ios::out | std::ios::app);
-     //   std::string filename = assembly->GetName() + ": ";
-     //   myfile << filename;
-     //   myfile << overlaps << "\n";
-     //   myfile.close();
+       // std::cout << "Time accessible for " << converted_resnum << ": " <<  percent_accessible << std::endl;
     }
 
-    CalculateAtomicOverlapsForAssemblyVector(&enzyme, substrate_snapshots);
-*/
     return 0;
 }
-
-
-/*bool CheckIfProtein(std::string resname){
-    std::vector<std::string> residue_list = {"ALA","ASP", "ASN", "ARG", "GLY", "GLU", "GLN", "PRO", "HIS", "CYS", "VAL", "LEU", "THR", "SER", "LYS", "MET", "TYR", "TRP", "PHE", "SEC", "ILE", "CYX", "HID", "HIE" };
-    //std::string resname = this->GetName();
-    if (std::any_of(residue_list.begin(), residue_list.end(),
-                [resname](std::string residue_names) {
-        if (residue_names.compare(resname)==0)
-            return true;
-        return false;
-    }))
-        return true;
-    return false;
-}*/
 
 double SuperimposeSnapshotsToEnzymeCalculatePercentTimeAccessible(Assembly *enzyme, Assembly *enzyme_ligand, AssemblyVector substrate_snapshots, std::string substrate_residue_number)
 {
@@ -274,18 +215,15 @@ double SuperimposeSnapshotsToEnzymeCalculatePercentTimeAccessible(Assembly *enzy
             }
         }
     }
-
-    int snapshot_accessible_counter = 0;
-
     // This next part is for running on multiple CPUs
     pid_t pids[substrate_snapshots.size()];
     int i;
    // unsigned concurentThreadsSupported = std::thread::hardware_concurrency();
    // std::cout << "Number of cores: " << concurentThreadsSupported << std::endl; // Not using now, try optimize with this in future.
     int n = substrate_snapshots.size();
-
-    std::cout << "Starting " << n << " children" << std::endl;
+    //std::cout << "Starting " << n << " children" << std::endl;
     /* Start children. */
+   // double total_overlap = 0.0;
     for (i = 0; i < n; ++i)
     {
         if ((pids[i] = fork()) < 0)
@@ -298,7 +236,8 @@ double SuperimposeSnapshotsToEnzymeCalculatePercentTimeAccessible(Assembly *enzy
             //DoWorkInChild();
             AtomVector super_atoms = GetAtomSelection(substrate_snapshots.at(i), residue_selection_strings, atom_selection_strings );
             AtomVector substrate_all = substrate_snapshots.at(i)->GetAllAtomsOfAssembly();
-            AtomVector substrate_protein = substrate_snapshots.at(i)->GetAllAtomsOfAssemblyWithinProteinResidues();
+            //AtomVector substrate_protein = substrate_snapshots.at(i)->GetAllAtomsOfAssemblyWithinProteinResidues();
+
             //std::cout << "Substrate_protein.size " << substrate_protein.size() << std::endl;
            // substrate_snapshots.at(i)->BuildStructureByDistance(4);
             //substrate_snapshots.at(i)->BuildStructureByDistanceByOptimizedThread();
@@ -309,67 +248,62 @@ double SuperimposeSnapshotsToEnzymeCalculatePercentTimeAccessible(Assembly *enzy
           //  std::cout << "X value is now " << substrate_protein.at(0)->GetCoordinates().at(0)->GetX() << std::endl;
           //  std::cout << "Original is now " << substrate_snapshots.at(i)->GetAllAtomsOfAssembly().at(0)->GetCoordinates().at(0)->GetX() << std::endl;
 
+            // So any atom within the target atoms, and all subsrate atoms within 30 angstrom. All others are too far and slow overlap calculation.
+            AtomVector substrate_atoms_within30 = substrate_snapshots.at(i)->GetAllAtomsOfAssemblyWithinXAngstromOf(target_atoms.at(0)->GetCoordinates().at(0), 30);
+            AtomVector substrate_backbone = substrate_snapshots.at(i)->GetAllAtomsOfAssemblyWithinProteinBackbone();
+            AtomVector subsrate_backbone_within30 = GetAtomsCommonToBothAtomVectors(substrate_atoms_within30, substrate_backbone);
+
+            /*
             substrate_snapshots.at(i)->BuildStructureByDistance(4);
             std::stringstream ss;
             ss << "outputs/moved_" << i << "_" << substrate_residue_number << ".pdb";
             PdbFileSpace::PdbFile *outputPdbFile = substrate_snapshots.at(i)->BuildPdbFileStructureFromAssembly(-1,0);
             outputPdbFile->Write(ss.str());
+            */
 
 
-            double current_overlap = enzyme->CalculateAtomicOverlaps(substrate_protein);
+            double current_overlap = enzyme->CalculateAtomicOverlaps(subsrate_backbone_within30);
+           // total_overlap += current_overlap;
+
             std::cout << "enzyme overlap with substrate is " << current_overlap << std::endl;
             //double current_overlap = SuperimposeEnzymeCalculateOverlaps(enzyme, target_atoms, substrate, super_atoms);
             if (current_overlap < 2.5 )
             {
-                ++snapshot_accessible_counter;
+                exit(1);
+               // std::cout << "Address of counter is " << ptr_snapshot_accessible_counter << std::endl;
+              //  *ptr_snapshot_accessible_counter = (*ptr_snapshot_accessible_counter + 1);
+              //  std::cout << "Counter is " << *ptr_snapshot_accessible_counter << std::endl;
             }
             exit(0);
         }
     }
 
+    int snapshot_accessible_counter = 0;
     /* Wait for children to exit. */
     int status;
     pid_t pid;
     while (n > 0)
     {
         pid = wait(&status);
-        //  printf("Child with PID %ld exited with status 0x%x.\n", (long)pid, status);
+          //printf("Child with PID %ld exited with status 0x%x.\n", (long)pid, status);
+        //std::cout << "Exit with status of " << status << std::endl;
+        if(status != 0 )
+        {
+            snapshot_accessible_counter++;
+        }
         --n;  // TODO(pts): Remove pid from the pids array.
     }
 
-    /*i = 0;
-    for(AssemblyVector::iterator it = substrate_snapshots.begin(); it != substrate_snapshots.end(); ++it)
-    {
-        Assembly *substrate = *it;
-        std::cout << "X value will now " << substrate->GetAllAtomsOfAssembly().at(0)->GetCoordinates().at(0)->GetX() << std::endl;
-        //thread this:
-
-       // futures_returns.push_back(std::async(&SuperimposeEnzymeCalculateOverlaps, enzyme, target_atoms, substrate, substrate_residue_number));
-
-
-        //   double current_overlap = ret.get();
-        //if (ret.get() < 2.5) { ++snapshot_accessible_counter; }
-        // std::cout << "enzyme overlap with substrate is " << ret.get() << std::endl;
-        //double current_overlap = SuperimposeEnzymeCalculateOverlaps(enzyme, target_atoms, substrate, substrate_residue_number);
-        //  std::cout << "enzyme overlap with substrate is " << current_overlap << std::endl;
-
-     //   if (current_overlap < 2.5 )
-     //   {
-     //       ++snapshot_accessible_counter;
-    //    }
-        //Write out a pdb file:
-        substrate->BuildStructureByDistance(4);
-        std::stringstream ss;
-        ss << "outputs/moved_" << i << ".pdb";
-        PdbFileSpace::PdbFile *outputPdbFile = substrate->BuildPdbFileStructureFromAssembly(-1,0);
-        outputPdbFile->Write(ss.str());
-        ++i;
-    }*/
+   // std::cout << "snapshot_accessible_counter: " << snapshot_accessible_counter << std::endl;
 
     if ( (snapshot_accessible_counter > 0) && (substrate_snapshots.size() > 0) )
-        return ( (snapshot_accessible_counter/substrate_snapshots.size() ) * 100 );
+    {
+        double percent = ( (snapshot_accessible_counter / double(substrate_snapshots.size()) ) * 100 );
+        //std::cout << "Returning " << percent  << std::endl;
+        return percent;
+    }
     else
-        return 0;
+        return 0.0;
 }
 
 AtomVector GetAtomSelection(Assembly *assembly, StringVector residue_numbers, StringVector atom_names)
@@ -445,13 +379,13 @@ MolecularModeling::Residue* seek_alycone_residue_connected_to_residue_number(std
 MolecularModeling::Residue* recursively_seek_alycone_residue(Atom *start_atom, bool *found)
 {
     Residue *aglycone = new Residue();
-    start_atom->SetDescription("Visited"); // Hmm. Not sure this is a good idea, but it sure is handy.
+    start_atom->SetDescription("VisitedByAglyconSeeker"); // Hmm. Not sure this is a good idea, but it sure is handy.
     //std::cout << "Checking neighbors of " << start_atom->GetId() << std::endl;
     AtomVector neighbors = start_atom->GetNode()->GetNodeNeighbors();
     for(AtomVector::iterator it = neighbors.begin(); it != neighbors.end(); ++it)
     {
         // If haven't found aglycone yet and haven't visited this neighbor already
-        if ( (*found == false) && ((*it)->GetDescription().compare("Visited")!=0) )
+        if ( (*found == false) && ((*it)->GetDescription().compare("VisitedByAglyconSeeker")!=0) )
         {
             Atom *neighbor = *it;
             if(neighbor->GetResidue()->CheckIfProtein())
@@ -467,6 +401,86 @@ MolecularModeling::Residue* recursively_seek_alycone_residue(Atom *start_atom, b
     }
     return aglycone;
 }
+
+AtomVector seek_attached_residues_that_are_not_protein(std::string residue_number, Assembly *assembly)
+{
+    //First find the residue in the assembly using the residue name.
+    MolecularModeling::Residue *current_residue = find_residue_by_number(residue_number, assembly);
+    //Now find any atom to start the search from. There is no residue connection graph right now.
+    Atom *start_atom = current_residue->GetAtoms().at(0);
+    //Now iteratively search through neighbors and find them all. Skip when it hits protein.
+    AtomVector returned_selection;
+    recursively_find_attached_residues_that_are_not_protein(start_atom, returned_selection);
+    return returned_selection;
+}
+
+void recursively_find_attached_residues_that_are_not_protein(Atom *start_atom, AtomVector &returned_selection)
+{
+    start_atom->SetDescription("VisitedByGlycanMoleculeSeeker"); // Hmm. Not sure this is a good idea, but it sure is handy.
+    AtomVector neighbors = start_atom->GetNode()->GetNodeNeighbors();
+    //std::cout << "Checking neighbors of " << start_atom->GetId() << std::endl;
+    for(AtomVector::iterator it = neighbors.begin(); it != neighbors.end(); ++it)
+    {
+        if ((*it)->GetDescription().compare("VisitedByGlycanMoleculeSeeker")!=0) // If haven't visited this neighbor already
+        {
+            Atom *neighbor = *it;
+            if(neighbor->GetResidue()->CheckIfProtein())
+            {
+              //std::cout << "Hit the aglycon; skipping" << std::endl;
+            }
+            else
+            {
+                returned_selection.push_back(neighbor);
+                recursively_find_attached_residues_that_are_not_protein(neighbor, returned_selection);
+            }
+        }
+    }
+    return;
+}
+
+AtomVector GetAtomsCommonToBothAtomVectors(AtomVector a, AtomVector b)
+{
+    AtomVector returned_selection;
+    for(AtomVector::iterator it = a.begin(); it != a.end(); ++it)
+    {
+        Atom *atom_a = *it;
+        for(AtomVector::iterator it1 = b.begin(); it1 != b.end(); ++it1)
+        {
+            Atom *atom_b = *it1;
+            if (atom_a->GetIndex() == atom_b->GetIndex())
+            {
+                returned_selection.push_back(atom_a);
+            }
+        }
+    }
+    return returned_selection;
+}
+
+AtomVector GetAtomsin_a_Notin_b_AtomVectors(AtomVector a, AtomVector b)
+{
+    AtomVector returned_selection;
+    bool isCommon = false;
+    for(AtomVector::iterator it = a.begin(); it != a.end(); ++it)
+    {
+        isCommon = false;
+        Atom *atom_a = *it;
+        for(AtomVector::iterator it1 = b.begin(); it1 != b.end(); ++it1)
+        {
+            Atom *atom_b = *it1;
+            if (atom_a->GetIndex() == atom_b->GetIndex())
+            {
+                isCommon = true;
+            }
+        }
+        if (isCommon==false)
+        {
+            returned_selection.push_back(atom_a);
+        }
+    }
+    return returned_selection;
+}
+
+
 
 //double SuperimposeEnzymeCalculateOverlaps(Assembly *enzyme, AtomVector target_atoms, AtomVector alsoMoving_atoms, AtomVector substrate_superimposition_atoms)
 //{
